8462b3b6d899b7eaf12bf52b6adde9c2daca3219
jamesMSJ <together5616@gmail.com>
2024-07-19 16:25:44 +0900

    수수료계산법 수정

--- script.js
+++ script.js
@@ -10,27 +10,26 @@ $(document).ready(function () {
         var useRentalCar = $('#useRentalCar').is(':checked');
 
         var feePerPlayer = 10000; // 골프 인원 1인당 1만원
-        var feePerRound = 10000;  // 라운딩 1회당 1만원
         var feePerNight = 10000;  // 1박당 1만원
-        var feePerRoom = 10000;   // 방 1개당 1만원
         var rentalCarFee = 10000; // 렌트카 사용시 1만원
 
         // 골프 수수료는 인원 * 라운딩 횟수 * 1만원
         var golfFee = numPlayers * numRounds * feePerPlayer;
+
+        var discountApplied = false;
+
+        // 골프 인원 8명 이상 또는 숙박 일수 10일 이상일 때 할인 적용
+        if (numPlayers >= 8 || numNights >= 10) {
+            golfFee *= 0.7; // 골프 및 라운딩 횟수에 따른 금액의 30% 할인 적용
+            discountApplied = true;
+        }
+
         // 숙박 수수료는 1박당 1만원 * 방 갯수
         var lodgingFee = numNights * numRooms * feePerNight;
         var totalFee = golfFee + lodgingFee;
 
         if (useRentalCar) {
             totalFee += rentalCarFee;
-        }
-
-        var discountApplied = false;
-
-        // 할인 적용
-        if (numPlayers >= 8 || numNights >= 10) {
-            totalFee *= 0.7; // 30% 할인 적용
-            discountApplied = true;
         }
 
         $('#result').text("전체 수수료: " + totalFee.toLocaleString() + " 원");
